94162383f17f8c8b27a3cfff23134f07f544614f
roderickzxy <roderickzxy@qq.com>
2017-11-09 13:24:15 +0800

    add the eureka client to user service.

--- microservice-provider-user/src/main/java/com/rode/cloud/MicroserviceSimpleProviderUserApplication.java
+++ microservice-provider-user/src/main/java/com/rode/cloud/MicroserviceSimpleProviderUserApplication.java
@@ -2,11 +2,13 @@ package com.rode.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * Test application
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class MicroserviceSimpleProviderUserApplication {
 
 	public static void main(String[] args) {
