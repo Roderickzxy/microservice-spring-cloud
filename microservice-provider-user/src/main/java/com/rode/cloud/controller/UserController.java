3e0ee6a01827aa7de8995df37137ec50ec70c5db
roderickzxy <roderickzxy@qq.com>
2017-11-26 18:33:21 +0800

    register movie service to eureka server.

--- microservice-provider-user/src/main/java/com/rode/cloud/controller/UserController.java
+++ microservice-provider-user/src/main/java/com/rode/cloud/controller/UserController.java
@@ -1,16 +1,41 @@
 package com.rode.cloud.controller;
 
+import com.netflix.appinfo.InstanceInfo;
+
+import com.netflix.discovery.EurekaClient;
 import com.rode.cloud.entity.User;
 import com.rode.cloud.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 @RestController
 public class UserController {
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private EurekaClient eurekaClient;
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @GetMapping("/eureka-instance")
+    public String serviceUrl() {
+        InstanceInfo instance = eurekaClient.getNextServerFromEureka("microservice-provider-user", false);
+        return instance.getHomePageUrl();
+    }
+
+    @GetMapping("/instance-info")
+    public ServiceInstance showInfo(){
+        ServiceInstance localServiceInstance = this.discoveryClient.getLocalServiceInstance();
+        return localServiceInstance;
+    }
 
     @GetMapping("/user/{id}")
     public User getUser(@PathVariable("id")Long id){
